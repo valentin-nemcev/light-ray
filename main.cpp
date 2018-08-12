4d9273adbfb9d547bbad1ef231f3c33924950751
Валентин Немцев <v-nemcev@yandex-team.ru>
2018-08-13 02:21:06 +0300

    Replace SDL_Delay with event loop
    Now, the only event we handle is `SDL_QUIT`, so the close window button
    works as expected

--- main.cpp
+++ main.cpp
@@ -12,18 +12,31 @@ int main(int /*argc*/, char * /*args*/ []) {
     fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
     return 1;
   }
-  window = SDL_CreateWindow("hello_sdl2", SDL_WINDOWPOS_UNDEFINED,
-                            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                            SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  window = SDL_CreateWindow("Light Ray",
+                            SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED,
+                            SCREEN_WIDTH,
+                            SCREEN_HEIGHT,
+                            SDL_WINDOW_SHOWN);
   if (window == nullptr) {
     fprintf(stderr, "could not create window: %s\n", SDL_GetError());
     return 1;
   }
   screenSurface = SDL_GetWindowSurface(window);
-  SDL_FillRect(screenSurface, nullptr,
+  SDL_FillRect(screenSurface,
+               nullptr,
                SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
   SDL_UpdateWindowSurface(window);
-  SDL_Delay(2000);
+
+  for (bool running = true; running;) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event) != 0) {
+      if (event.type == SDL_QUIT) {
+        running = false;
+      }
+    }
+  }
+
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
