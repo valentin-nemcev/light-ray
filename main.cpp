5e2cccf5a5b306a7e860ca3a4ac66577dbdaef04
Валентин Немцев <v-nemcev@yandex-team.ru>
2018-08-28 02:15:30 +0300

    Move SDL stuff into its own class

--- main.cpp
+++ main.cpp
@@ -1,53 +1,75 @@
-#include <iostream>
-
 #include <SDL2/SDL.h>
+#include <boost/format.hpp>
+#include <iostream>
+#include <stdexcept>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-void setPixel(SDL_Surface *surface, int x, int y, int value) {
-    const SDL_Rect rect = {.x = x, .y = y, .w = 1, .h = 1};
-    SDL_FillRect(
-        surface, &rect, SDL_MapRGB(surface->format, value, value, value));
-}
-
-int main(int /*argc*/, char * /*args*/ []) {
+class Display {
     SDL_Window *window = nullptr;
     SDL_Surface *screenSurface = nullptr;
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
-        return 1;
+
+    void sdlError(std::string message) {
+        throw std::runtime_error(
+            boost::str(boost::format("%s: %s\n") % message % SDL_GetError()));
     }
-    window = SDL_CreateWindow("Light Ray",
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SCREEN_WIDTH,
-                              SCREEN_HEIGHT,
-                              SDL_WINDOW_SHOWN);
-    if (window == nullptr) {
-        fprintf(stderr, "could not create window: %s\n", SDL_GetError());
-        return 1;
+
+  public:
+    Display() {
+        if (SDL_Init(SDL_INIT_VIDEO) > 0) {
+            sdlError("Could not initialize SDL2");
+        }
+
+        window = SDL_CreateWindow("Light Ray",
+                                  SDL_WINDOWPOS_UNDEFINED,
+                                  SDL_WINDOWPOS_UNDEFINED,
+                                  SCREEN_WIDTH,
+                                  SCREEN_HEIGHT,
+                                  SDL_WINDOW_SHOWN);
+        if (window == nullptr) {
+            sdlError("Could not create window");
+        }
+
+        screenSurface = SDL_GetWindowSurface(window);
     }
-    screenSurface = SDL_GetWindowSurface(window);
+
+    void setPixel(int x, int y, int value) {
+        const SDL_Rect rect = {.x = x, .y = y, .w = 1, .h = 1};
+        SDL_FillRect(screenSurface,
+                     &rect,
+                     SDL_MapRGB(screenSurface->format, value, value, value));
+    }
+
+    void update() { SDL_UpdateWindowSurface(window); }
+
+    void waitForQuit() {
+        for (bool running = true; running;) {
+            SDL_Event event;
+            while (SDL_PollEvent(&event) != 0) {
+                if (event.type == SDL_QUIT) {
+                    running = false;
+                }
+            }
+        }
+
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+};
+
+int main(int /*argc*/, char * /*args*/ []) {
+    Display display;
 
     for (int x = 0; x < SCREEN_WIDTH; x++) {
         for (int y = 0; y < SCREEN_HEIGHT; y++) {
             const Uint8 value = ((x / 20) % 2) ^ ((y / 20) % 2);
-            setPixel(screenSurface, x, y, value * 255);
-        }
-    }
-    SDL_UpdateWindowSurface(window);
-
-    for (bool running = true; running;) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event) != 0) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
+            display.setPixel(x, y, value * 255);
         }
     }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    display.update();
+    display.waitForQuit();
+
     return 0;
 }
