93231a1f0cd88ff94fea574a6b157a171391077f
Valentin Nemcev <valentin.nemcev@smartly.io>
2020-04-23 22:54:38 +0300

    Improve build

--- build.sh
+++ build.sh
@@ -3,12 +3,31 @@
 # Exit on error
 set -e
 
+for i in "$@"
+do
+case $i in
+    --setup)
+    SETUP=YES
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+
 export CC="$(brew --prefix llvm)/bin/clang"
 export CXX="$(brew --prefix llvm)/bin/clang++"
+export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
 
 cd build
 
-# cmake ..
+if [ ! -z $SETUP ]
+then
+  cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+  mv compile_commands.json ..
+fi
+
 cmake --build .
 
 ./main
