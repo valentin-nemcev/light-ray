da72f1b152e08122f34b849ffe84b9da7e227b8a
Valentin Nemcev <valentin.nemcev@smartly.io>
2020-05-11 12:03:47 +0300

    Setup clang-tidy and fix naming

--- main.cpp
+++ main.cpp
@@ -22,9 +22,9 @@ using Eigen::Vector3d;
 
 using Pixel = Eigen::Matrix<unsigned int, 2, 1>;
 
-Eigen::IOFormat VFmt(Eigen::StreamPrecision, 0, ", ", ";", "", "", "(", ")");
+Eigen::IOFormat v_fmt(Eigen::StreamPrecision, 0, ", ", ";", "", "", "(", ")");
 
-Vector3d directionUp(0, 0, 1);
+Vector3d direction_up(0, 0, 1);
 
 struct Ray {
   const Vector3d origin;
@@ -39,7 +39,7 @@ struct RayBounce {
   bool bounced() { return distance >= 0; }
 };
 
-static const RayBounce noBounce = {.distance = -1};
+static const RayBounce no_bounce = {.distance = -1};
 
 class Object {
 public:
@@ -64,7 +64,7 @@ public:
                                 (ray.origin - pos).squaredNorm() +
                                 pow(radius, 2);
     if (discriminant < 0) {
-      return noBounce;
+      return no_bounce;
     }
 
     double distance = -ray.direction.dot(ray.origin - pos) - sqrt(discriminant);
@@ -99,15 +99,15 @@ public:
 
 using Scene = std::vector<std::unique_ptr<Object>>;
 
-void randomlyRotate(Vector3d &v) {
+void randomly_rotate(Vector3d &v) {
   static std::default_random_engine e;
-  static std::uniform_real_distribution<> randomScalar(0, 1);
+  static std::uniform_real_distribution<> random_scalar(0, 1);
 
-  Vector3d randomVector(randomScalar(e), randomScalar(e), randomScalar(e));
+  Vector3d random_vector(random_scalar(e), random_scalar(e), random_scalar(e));
 
-  randomVector -= randomVector.dot(v) * v; // make it orthogonal to v
+  random_vector -= random_vector.dot(v) * v; // make it orthogonal to v
 
-  v += randomVector;
+  v += random_vector;
   v.normalize();
 }
 
@@ -128,11 +128,11 @@ struct PixelValue {
     b /= count;
   }
 
-  int intR() const { return static_cast<int>(255.0 * r); }
+  int int_r() const { return static_cast<int>(255.0 * r); }
 
-  int intG() const { return static_cast<int>(255.0 * g); }
+  int int_g() const { return static_cast<int>(255.0 * g); }
 
-  int intB() const { return static_cast<int>(255.0 * b); }
+  int int_b() const { return static_cast<int>(255.0 * b); }
 };
 
 struct CameraPixel {
@@ -148,25 +148,25 @@ struct CameraPixel {
   }
 
   PixelValue trace(Scene const &scene) {
-    RayBounce closestBounce = noBounce;
+    RayBounce closest_bounce = no_bounce;
     for (auto &shape : scene) {
       RayBounce bounce = shape->intersect(ray);
-      if (bounce.bounced() && (!closestBounce.bounced() ||
-                               bounce.distance < closestBounce.distance)) {
-        closestBounce = bounce;
+      if (bounce.bounced() && (!closest_bounce.bounced() ||
+                               bounce.distance < closest_bounce.distance)) {
+        closest_bounce = bounce;
       }
     }
 
-    randomlyRotate(closestBounce.normal);
+    randomly_rotate(closest_bounce.normal);
 
     PixelValue value;
 
-    if (closestBounce.bounced()) {
-      double hit = exp(-closestBounce.distance * 0.25);
+    if (closest_bounce.bounced()) {
+      double hit = exp(-closest_bounce.distance * 0.25);
       // double hit = pow(cos(closestBounce.distance * 20), 64);
-      value.r = hit * (closestBounce.normal.x() / 2 + 0.5);
-      value.g = hit * (closestBounce.normal.y() / 2 + 0.5);
-      value.b = hit * (closestBounce.normal.z() / 2 + 0.5);
+      value.r = hit * (closest_bounce.normal.x() / 2 + 0.5);
+      value.g = hit * (closest_bounce.normal.y() / 2 + 0.5);
+      value.b = hit * (closest_bounce.normal.z() / 2 + 0.5);
     } else {
       value.r = value.g = value.b = 0;
     }
@@ -179,29 +179,29 @@ using Pixels = std::vector<CameraPixel>;
 
 class Camera {
 private:
-  CameraPixel indexToPixel(const unsigned int index) const {
-    const Pixel pixel(index % imageSize.x(), index / imageSize.x());
-    const double aspectRatio =
-        static_cast<double>(imageSize.x()) / imageSize.y();
-    const Vector2d pixelPos((pixel.x() + 0.5) / imageSize.x() - 0.5,
-                            (pixel.y() + 0.5) / imageSize.y() / aspectRatio -
-                                0.5);
+  CameraPixel index_to_pixel(const unsigned int index) const {
+    const Pixel pixel(index % image_size.x(), index / image_size.x());
+    const double aspect_ratio =
+        static_cast<double>(image_size.x()) / image_size.y();
+    const Vector2d pixel_pos((pixel.x() + 0.5) / image_size.x() - 0.5,
+                             (pixel.y() + 0.5) / image_size.y() / aspect_ratio -
+                                 0.5);
 
     Vector3d direction = (target - pos).normalized();
-    const Vector3d cameraRight = direction.cross(directionUp);
-    const Vector3d cameraUp = cameraRight.cross(direction);
-    Vector3d rayDirection =
-        (direction + cameraRight * pixelPos.x() - cameraUp * pixelPos.y())
+    const Vector3d camera_right = direction.cross(direction_up);
+    const Vector3d camera_up = camera_right.cross(direction);
+    Vector3d ray_direction =
+        (direction + camera_right * pixel_pos.x() - camera_up * pixel_pos.y())
             .normalized();
-    return {.pixel = pixel, .ray = {.origin = pos, .direction = rayDirection}};
+    return {.pixel = pixel, .ray = {.origin = pos, .direction = ray_direction}};
   }
 
-  Pixels allocatePixels() const {
+  Pixels allocate_pixels() const {
     Pixels pixels;
-    const Pixel::Scalar pixelCount = imageSize.x() * imageSize.y();
-    pixels.reserve(pixelCount);
-    for (int i = 0; i < pixelCount; i++) {
-      pixels.push_back(indexToPixel(i));
+    const Pixel::Scalar pixel_count = image_size.x() * image_size.y();
+    pixels.reserve(pixel_count);
+    for (int i = 0; i < pixel_count; i++) {
+      pixels.push_back(index_to_pixel(i));
     }
     return pixels;
   }
@@ -209,40 +209,40 @@ private:
 public:
   const Vector3d pos;
   const Vector3d target;
-  const Pixel imageSize;
+  const Pixel image_size;
   Pixels pixels;
 
   Camera(Vector3d const initial_pos, Vector3d const initial_target,
-         Pixel const initial_imageSize)
-      : pos(initial_pos), target(initial_target), imageSize(initial_imageSize),
-        pixels(allocatePixels()) {}
+         Pixel const initial_image_size)
+      : pos(initial_pos), target(initial_target),
+        image_size(initial_image_size), pixels(allocate_pixels()) {}
 };
 
 class Caster {
-  std::span<CameraPixel> pixelSpan;
+  std::span<CameraPixel> pixel_span;
   const Scene &scene;
 
   std::thread thread;
 
   void render() {
-    for (auto &pixel : pixelSpan) {
+    for (auto &pixel : pixel_span) {
       pixel.render(scene);
     }
-    isRendering = false;
+    is_rendering = false;
   }
 
 public:
-  bool isRendering = false;
+  bool is_rendering = false;
 
-  Caster(std::span<CameraPixel> _pixelSpan, Scene const &_scene)
-      : pixelSpan(_pixelSpan), scene(_scene) {}
+  Caster(std::span<CameraPixel> pixel_span, Scene const &scene)
+      : pixel_span(pixel_span), scene(scene) {}
 
-  void startRendering() {
-    isRendering = true;
+  void start_rendering() {
+    is_rendering = true;
     thread = std::thread(&Caster::render, this);
   }
 
-  void waitUntilRendered() {
+  void wait_until_rendered() {
     if (thread.joinable())
       thread.join();
   }
@@ -250,31 +250,31 @@ public:
 
 class Display {
   SDL_Window *window = nullptr;
-  SDL_Surface *screenSurface = nullptr;
+  SDL_Surface *screen_surface = nullptr;
 
-  void sdlError(std::string message) {
+  void sdl_error(std::string message) {
     throw std::runtime_error(
         boost::str(format("%s: %s\n") % message % SDL_GetError()));
   }
 
-  std::chrono::time_point<std::chrono::system_clock> measureStartTime;
+  std::chrono::time_point<std::chrono::system_clock> measure_start_time;
 
 public:
-  bool isRunning = true;
+  bool is_running = true;
 
   Display() {
     if (SDL_Init(SDL_INIT_VIDEO) > 0) {
-      sdlError("Could not initialize SDL2");
+      sdl_error("Could not initialize SDL2");
     }
 
     window = SDL_CreateWindow("Light Ray", SDL_WINDOWPOS_UNDEFINED,
                               SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
                               SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == nullptr) {
-      sdlError("Could not create window");
+      sdl_error("Could not create window");
     }
 
-    screenSurface = SDL_GetWindowSurface(window);
+    screen_surface = SDL_GetWindowSurface(window);
   }
 
   ~Display() {
@@ -283,48 +283,50 @@ public:
     SDL_Quit();
   }
 
-  void drawPixels(Pixels const &pixels) {
+  void draw_pixels(Pixels const &pixels) {
     for (auto &pixel : pixels) {
-      if (!isRunning)
+      if (!is_running)
         break;
 
       const SDL_Rect rect = {.x = static_cast<int>(pixel.pixel.x()),
                              .y = static_cast<int>(pixel.pixel.y()),
                              .w = 1,
                              .h = 1};
-      SDL_FillRect(screenSurface, &rect,
-                   SDL_MapRGB(screenSurface->format, pixel.value.intR(),
-                              pixel.value.intG(), pixel.value.intB()));
+      SDL_FillRect(screen_surface, &rect,
+                   SDL_MapRGB(screen_surface->format, pixel.value.int_r(),
+                              pixel.value.int_g(), pixel.value.int_b()));
     }
   }
 
   void update() {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
-      processEvent(event);
+      process_event(event);
     }
 
     SDL_UpdateWindowSurface(window);
   }
 
-  void processEvent(SDL_Event &event) {
+  void process_event(SDL_Event &event) {
     if (event.type == SDL_QUIT) {
-      isRunning = false;
+      is_running = false;
     }
   }
 
-  void waitForEvent() {
+  void wait_for_event() {
     SDL_Event event;
     if (SDL_WaitEvent(&event)) {
-      processEvent(event);
+      process_event(event);
     }
   }
 
-  void startMeasure() { measureStartTime = std::chrono::system_clock::now(); }
+  void start_measure() {
+    measure_start_time = std::chrono::system_clock::now();
+  }
 
-  void completeMeasure(std::string const message) {
+  void complete_measure(std::string const message) {
 
-    const auto duration = std::chrono::system_clock::now() - measureStartTime;
+    const auto duration = std::chrono::system_clock::now() - measure_start_time;
 
     const auto ms =
         std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
@@ -343,47 +345,47 @@ int main(int /*argc*/, char * /*args*/[]) {
 
   scene.reserve(4);
 
-  scene.push_back(std::make_unique<Plane>(Vector3d(0, 0, 0), directionUp));
+  scene.push_back(std::make_unique<Plane>(Vector3d(0, 0, 0), direction_up));
   scene.push_back(std::make_unique<Sphere>(Vector3d(0.3, 0.3, 0.3), 0.3));
   scene.push_back(std::make_unique<Sphere>(Vector3d(0, -0.5, 0.5), 0.5));
   scene.push_back(std::make_unique<Sphere>(Vector3d(-0.6, 0, 0.6), 0.6));
 
   display.update();
 
-  display.startMeasure();
+  display.start_measure();
 
-  const auto threadCount = std::thread::hardware_concurrency();
-  const auto pixelsPerCaster =
-      std::div(static_cast<int>(camera.pixels.size()), threadCount);
+  const auto thread_count = std::thread::hardware_concurrency();
+  const auto pixels_per_caster =
+      std::div(static_cast<int>(camera.pixels.size()), thread_count);
 
   std::vector<Caster> casters;
-  casters.reserve(threadCount);
-  for (int i = 0; i < threadCount; i++) {
-    const int start = pixelsPerCaster.quot * i;
-    const int count =
-        pixelsPerCaster.quot + (i == threadCount - 1 ? pixelsPerCaster.rem : 0);
+  casters.reserve(thread_count);
+  for (int i = 0; i < thread_count; i++) {
+    const int start = pixels_per_caster.quot * i;
+    const int count = pixels_per_caster.quot +
+                      (i == thread_count - 1 ? pixels_per_caster.rem : 0);
     casters.emplace_back(
         std::span<CameraPixel>(camera.pixels).subspan(start, count),
         std::cref(scene));
-    casters[i].startRendering();
+    casters[i].start_rendering();
   }
 
-  while (display.isRunning) {
-    bool isRendering = false;
+  while (display.is_running) {
+    bool is_rendering = false;
     for (auto &caster : casters)
-      isRendering = isRendering || caster.isRendering;
-    display.drawPixels(camera.pixels);
+      is_rendering = is_rendering || caster.is_rendering;
+    display.draw_pixels(camera.pixels);
     display.update();
-    if (!isRendering)
+    if (!is_rendering)
       break;
   }
 
   for (auto &caster : casters)
-    caster.waitUntilRendered();
-  display.completeMeasure("Rendered in ");
+    caster.wait_until_rendered();
+  display.complete_measure("Rendered in ");
 
-  while (display.isRunning)
-    display.waitForEvent();
+  while (display.is_running)
+    display.wait_for_event();
 
   return 0;
 }
